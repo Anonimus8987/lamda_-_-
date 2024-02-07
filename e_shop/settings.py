cc65ebf483641c41a157b388f9ac81e51e517bbc
Anonimus8987 <abdulazizsobitov2004@gmail.com>
2024-02-07 23:42:00 +0500

    hello

--- e_shop/settings.py
+++ e_shop/settings.py
@@ -76,18 +76,18 @@ WSGI_APPLICATION = 'e_shop.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
     # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': 'shop',
-    #     'USER': 'postgres',
-    #     'PORT': 5432,
-    #     'PASSWORD': 'pass',
-    #     'HOST': 'localhost'
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'shop',
+        'USER': 'postgres',
+        'PORT': 5432,
+        'PASSWORD': 'poker3301',
+        'HOST': 'localhost'
+    }
 }
 
 
